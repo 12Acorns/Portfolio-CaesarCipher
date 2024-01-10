74af1ef821cbb56c982b8b5913b55aac28df4f23
12Acorns <nathanrutter09@gmail.com>
2024-01-10 20:04:53 +0000

    Commit

---
+++ CaesarCipherApp/CaesarCipher.cs
@@ -0,0 +1,27 @@
+﻿namespace CaesarCipherApp
+{
+	public static class CaesarCipher
+	{
+		public static unsafe string BasicEncryption(string _input, short _shiftCount)
+		{
+			if(_input is null)
+			{
+				return string.Empty;
+			}
+			else if(_input.Length <= 0)
+			{
+				return string.Empty;
+			}
+
+			Span<char> _encryptedString = stackalloc char[_input.Length];
+			for(int i = 0; i < _input.Length; i++)
+			{
+				_encryptedString[i] =
+					_input[i] == ' '
+					? ' '
+					: (char)(_input[i] + _shiftCount);
+			}
+			return _encryptedString.ToString();
+		}
+	}
+}
---
+++ CaesarCipherApp/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace CaesarCipherApp
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			string _result = CaesarCipher.BasicEncryption("hello world", 3);
+			Console.WriteLine(_result);
+			Console.ReadLine();
+		}
+	}
+}
